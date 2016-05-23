22b712b55bb9eab4cfda48a141e91e09ca37e61f
Nir Ben Amram <nbastyle@gmail.com>
2016-05-23 20:52:27 +0300

    added date input field to the room's form

--- calender/js/calendar.js
+++ calender/js/calendar.js
@@ -148,17 +148,17 @@ $(document).ready(function () {
 
 
 
-    // ///for a date picker
-    // $('#datepairExample .time').timepicker({
-    //     'showDuration': true,
-    //     'timeFormat': 'g:ia'
-    // });
-    //
-    // $('#datepairExample .date').datepicker({
-    //     'format': 'm/d/yyyy',
-    //     'autoclose': true
-    // });
-    //
-    // $('#datepairExample').datepair();
+     ///for a date picker
+     $('#datepairExample .time').timepicker({
+         'showDuration': true,
+         'timeFormat': 'g:ia'
+     });
+
+     $('#datepairExample .date').datepicker({
+         'format': 'm/d/yyyy',
+         'autoclose': true
+     });
+
+     $('#datepairExample').datepair();
 
 });
