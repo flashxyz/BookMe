7d2ef094538251c12cd2d1d50ca3f993274688e4
flashxyz <flashxyz@gmail.com>
2016-05-30 02:31:33 +0300

    Save room functionality is now working!

--- wp_book_me/admin/partials/save-rooms.php
+++ wp_book_me/admin/partials/save-rooms.php
@@ -7,16 +7,75 @@ global $wpdb;
 
 if($_GET['group_id']==true AND $_GET['room_id']==true AND  $_GET['save_room']==true){
 
+    //get the table for farther modification
+    $room_options_table = $wpdb->prefix . "bookme_rooms_options";
+
+    //get gata from GET\POST
     $groupID = $_GET['group_id'];
     $roomID = $_GET['room_id'];
     $postArray = $_POST['wp_book_me'];
+    
+    //get fields from submitted forms
+    $roomName = $postArray['roomOptionName_' . $roomID];
+    $roomCapacity = $postArray['roomOptionCapacity_' . $roomID];
 
-    echo "EditRoom";
-    echo $groupID;
-    echo $roomID;
+    //services not ready right now!.
+    //$roomServices = $postArray['roomOptionServices_' . $roomID];
+
+    $roomDescription = $postArray['roomOptionDescription_' . $roomID];
+    $roomIsActive = $postArray['roomOptionIsActive_' . $roomID];
+    $activeSundayCheckBox = $postArray['activeSunday_' . $roomID];
+    $activeMondayCheckBox = $postArray['activeMonday_' . $roomID];
+    $activeTuesdayCheckBox = $postArray['activeTuesday_' . $roomID];
+    $activeWednesdayCheckBox = $postArray['activeWednesday_' . $roomID];
+    $activeThursdayCheckBox = $postArray['activeThursday_' . $roomID];
+    $activeFridayCheckBox = $postArray['activeFriday_' . $roomID];
+    $activeSaturdayCheckBox = $postArray['activeSaturday_' . $roomID];
+
+
+    //parse the active days to TLV format
+    $activeDaysArray = serialize(array(
+        'sunday' => $activeSundayCheckBox,
+        'monday' => $activeMondayCheckBox,
+        'tuesday' => $activeTuesdayCheckBox,
+        'wednesday' => $activeWednesdayCheckBox,
+        'thursday' => $activeThursdayCheckBox,
+        'friday' => $activeFridayCheckBox,
+        'saturday' => $activeSaturdayCheckBox
+    ));
+
+
+    //create array of the data we want to insert to specific row
+    $dataArray = array(
+        'roomName' => $roomName,
+        'capacity' => $roomCapacity,
+        'activeDays' => $activeDaysArray,
+       //'services' => $roomServices,
+        'description' => $roomDescription,
+        'isActive' => $roomIsActive
+    );
+
+
+    //create array of the condition to get the specific row
+    $whereArray = array( 'roomId' => $roomID);
+
+    //execute the update function for saving data
+    $wpdb->update( $room_options_table, $dataArray, $whereArray);
+
+
+
+
 }
 
-
-
+//get the current path url
+$siteURL = get_site_url()."/wp-admin/admin.php";
 
 ?>
+
+<!-- this script redirect to the previous page-->
+<script>
+
+    var siteURL = <?php echo json_encode($siteURL);?>;
+    window.location.replace(siteURL + "?page=wp_book_me&group_id=<?php echo $groupID ?>&edit_rooms=true");
+
+</script>
