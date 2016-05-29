8a7a795102548eb653061e7fd0a6bc7aa0f61f8e
rotemsd <saadorotem@gmail.com>
2016-05-29 22:18:02 +0300

    update the number of rooms in group option table when deleting room

--- wp_book_me/admin/partials/delete-room.php
+++ wp_book_me/admin/partials/delete-room.php
@@ -15,6 +15,7 @@
         echo "Delete Room " . $roomID . "in Group " . $groupID;
 
         //get the table name we want to work with
+        $group_options_table = $wpdb->prefix . "bookme_group_options";
         $rooms_options_table = $wpdb->prefix . "bookme_rooms_options";
 
         //check if the row exist
@@ -24,6 +25,27 @@
         $wpdb->query( $wpdb->prepare( " DELETE FROM $rooms_options_table WHERE roomId = %d", $roomID));
 
 
+        //get all the rooms associate to this group id
+        $selectSQL = $wpdb->get_results( "SELECT * FROM $rooms_options_table WHERE groupId = '$groupID'" );
+
+
+        //update the number of rooms in group option table
+
+
+        //take the num of the rooms + 1
+        $numOfRooms  = sizeof($selectSQL);
+
+
+        //create array of the data we want to insert to specific row
+        $dataArray = array(
+            'numOfRooms' => $numOfRooms
+        );
+
+        //create array of the condition to get the specific row
+        $whereArray = array( 'id' => $groupID);
+
+        //execute the update function for saving data
+        $wpdb->update( $group_options_table, $dataArray, $whereArray);
 
     }
 
