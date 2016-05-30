ebd853fe12442184348e40e5ac868920c6c14f22
flashxyz <flashxyz@gmail.com>
2016-05-30 14:04:08 +0300

    Added Conver time unit test and function!

--- wp_book_me/public/js/file.js
+++ wp_book_me/public/js/file.js
@@ -5,3 +5,17 @@ function test1() {
     return true;
 }
 
+function convertTime(houreStart, minStart) {
+    var strTimeStart;
+    if (houreStart < 10)
+        if (minStart < 10)
+            strTimeStart = '0' + houreStart + ':' + minStart + '0';
+        else
+            strTimeStart = '0' + houreStart + ':' + minStart;
+    else if (minStart < 10)
+        strTimeStart = houreStart + ':' + minStart + '0';
+    else
+        strTimeStart = houreStart + ':' + minStart;
+    return strTimeStart;
+}
+
--- wp_book_me/public/test/cal_test.js
+++ wp_book_me/public/test/cal_test.js
@@ -10,6 +10,16 @@ QUnit.test( "Boolean test", function( assert ) {
 });
 
 
+QUnit.test( "convertTime test", function( assert ) {
+  assert.equal( convertTime(15,5), "05:50", "15,5 -> 05:50" );
+  assert.equal( convertTime(00,0), "00:00", "0,0 -> 00:00" );
+  assert.equal( convertTime(24,00), "", "24,0 -> "  );
+  assert.equal( convertTime(1,1), "1:10", "1,1 -> 01:10"  );
+  assert.equal( convertTime(01,01), "1:10", "01,01 -> 01:10"  );
+  assert.equal( convertTime('a','a'), "", "a,a -> "  );
+});
+
+
 
 
 
