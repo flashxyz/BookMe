17edade878cec6b618e6de58d621230e09f05069
flashxyz <flashxyz@gmail.com>
2016-05-30 13:58:58 +0300

    Added Unit Test basic functions

---
+++ wp_book_me/public/test/cal_test.js
@@ -0,0 +1,17 @@
+
+
+QUnit.test( "hello test", function( assert ) {
+  assert.ok( 1 == "1", "Passed!" );
+});
+
+
+QUnit.test( "Boolean test", function( assert ) {
+  assert.ok( test1() == true, "Passed!" );
+});
+
+
+
+
+
+
+
