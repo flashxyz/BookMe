25c9f0554eb1a0add22d1c42819f1ac645722803
Lior <liorsap1@gmail.com>
2016-06-09 18:22:53 +0300

    deleting the unesecery bootstrap modal

--- wp_book_me/public/partials/frontend-bookme.php
+++ wp_book_me/public/partials/frontend-bookme.php
@@ -415,27 +415,6 @@ $submitURL = get_site_url()."/wp-content/plugins/wp_book_me/public/partials/cale
                 </div>
             </div>
             <hr>
-            <div id="validationDialogWithUser">
-                <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Small Modal</button>
-
-                <!-- Modal -->
-                <div class="modal fade" id="myModal" role="dialog">
-                    <div class="modal-dialog modal-sm">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                <h4 class="modal-title">Modal Header</h4>
-                            </div>
-                            <div class="modal-body" id = "validOrderRoom">
-                                <p></p>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
     <!-- /.row -->
