37024e4eac9d7fbb434883489ffe823617d68959
Nir <nbastyle@gmail.com>
2016-06-17 13:51:10 +0300

    login link request, when the user is not logged in only

--- wp_book_me/public/partials/frontend-bookme.php
+++ wp_book_me/public/partials/frontend-bookme.php
@@ -19,28 +19,22 @@ $userID = get_current_user_id();
 $general_options_table = $wpdb->prefix . "bookme_general_options";
 
 //get general options from data
-$selectSQL_generalOptions = $wpdb->get_row( "SELECT * FROM $general_options_table" );
+$selectSQL_generalOptions = $wpdb->get_row("SELECT * FROM $general_options_table");
 
 $genOptIsRTL = $selectSQL_generalOptions->isRTL;
 $genOptFirstDay = $selectSQL_generalOptions->firstDayOfWeek;
 $genOptDateFormat = $selectSQL_generalOptions->dateFormat;
 
 //set rtl as full calendars rtl
-if($genOptIsRTL == '1')
-{
+if ($genOptIsRTL == '1') {
     $genOptIsRTL = "true";
-}
-else
-{
+} else {
     $genOptIsRTL = "false";
 }
 //set first day as full calendars first day
-if($genOptFirstDay == 'sunday')
-{
+if ($genOptFirstDay == 'sunday') {
     $genOptFirstDay = 0;
-}
-else
-{
+} else {
     $genOptFirstDay = 1;
 }
 
@@ -81,7 +75,7 @@ while ($index < $numberOfRooms) {
     $roomCell[0] = $selectSQL_rooms[$index]->roomId;
     $roomCell[1] = $selectSQL_rooms[$index]->roomName;
     $roomCell[2] = $selectSQL_rooms[$index]->capacity;
-    $roomCell[3] = implode(',', array_keys(unserialize($selectSQL_rooms[$index]->services),1));
+    $roomCell[3] = implode(',', array_keys(unserialize($selectSQL_rooms[$index]->services), 1));
     $roomCell[4] = $selectSQL_rooms[$index]->description;
     array_push($roomsArray, $roomCell);
     $roomArrayById[$roomCell[0]] = $roomCell[1];
@@ -109,7 +103,7 @@ while ($index < $numberOfReservation) {
     $index++;
 }
 
-$selectSQL_reservation_byUser = $wpdb->get_results($wpdb->prepare("SELECT * FROM $room_reservation_table WHERE userId = %d ",  $userID));
+$selectSQL_reservation_byUser = $wpdb->get_results($wpdb->prepare("SELECT * FROM $room_reservation_table WHERE userId = %d ", $userID));
 $numberOfReservationByUser = sizeof($selectSQL_reservation_byUser);
 
 //add all reservations for this user to an array
@@ -133,7 +127,7 @@ $services = unserialize($selectSQL[0]->services);
 $activeDays = unserialize($selectSQL[0]->activeDays);
 $groupName = $selectSQL[0]->groupName;
 $description = $selectSQL[0]->description;
-$reservationLimitation =  $selectSQL[0]->reservationLimitation;
+$reservationLimitation = $selectSQL[0]->reservationLimitation;
 
 //define a brighter color by 50%
 $brightness = 0.5; // 50% brighter
@@ -189,11 +183,9 @@ function colourBrightness($hex, $percent)
 
 
 //get the current path url
-$submitURL = get_site_url()."/wp-content/plugins/wp_book_me/public/partials/calendarSubmit.php";
+$submitURL = get_site_url() . "/wp-content/plugins/wp_book_me/public/partials/calendarSubmit.php";
 
-$searchRoomsURL = get_site_url()."/wp-content/plugins/wp_book_me/public/partials/searchRooms.php";
-
-
+$searchRoomsURL = get_site_url() . "/wp-content/plugins/wp_book_me/public/partials/searchRooms.php";
 
 
 ?>
@@ -221,8 +213,8 @@ $searchRoomsURL = get_site_url()."/wp-content/plugins/wp_book_me/public/partials
     var genOptDateFormat = "<?php echo $genOptDateFormat ?>";
     var genOptFirstDay = "<?php echo $genOptFirstDay ?>";
     //end of general options import -----------------
-    
-    
+
+
     var activeDays = [];
     activeDays[0] = "<?php echo $activeDays["sunday"] ?>";
     activeDays[1] = "<?php echo $activeDays["monday"] ?>";
@@ -243,7 +235,7 @@ $searchRoomsURL = get_site_url()."/wp-content/plugins/wp_book_me/public/partials
     i++;
     <?php } ?>
 
-    var numOfReservations= "<?php echo $numberOfReservationByUser ?>";
+    var numOfReservations = "<?php echo $numberOfReservationByUser ?>";
 
     //roomsArray = [];
 
@@ -251,16 +243,13 @@ $searchRoomsURL = get_site_url()."/wp-content/plugins/wp_book_me/public/partials
     echo "var roomsArray = " . $jsArray . ";\n";
     ?>
 
-    <?php $jsArray = json_encode( $reservation_array);
+    <?php $jsArray = json_encode($reservation_array);
     echo "var reservationsArray = " . $jsArray . ";\n";
     ?>
 
-    <?php $jsArray = json_encode( $reservation_array_byUser);
+    <?php $jsArray = json_encode($reservation_array_byUser);
     echo "var reservationsArrayByUser = " . $jsArray . ";\n";
     ?>
-
-    
-
 
 
 </script>
@@ -301,7 +290,7 @@ $searchRoomsURL = get_site_url()."/wp-content/plugins/wp_book_me/public/partials
 
     }
 
-    .dropdown:hover .dropbtn{
+    .dropdown:hover .dropbtn {
         background-color: <?php echo $calendarColor; ?> !important;
     }
 
@@ -331,17 +320,21 @@ $searchRoomsURL = get_site_url()."/wp-content/plugins/wp_book_me/public/partials
     }
 
     .cool-button {
-        background:  <?php echo $calendarColor; ?>;
+        background: <?php echo $calendarColor; ?>;
     }
+
     .cool-button:hover {
-        background:  <?php echo $fiftyPercentBrighter; ?> !important;
+        background: <?php echo $fiftyPercentBrighter; ?> !important;
     }
+
     .fc-bg {
         color: <?php echo $fiftyPercentBrighter; ?> !important;
     }
-    .fc-state-hover{
+
+    .fc-state-hover {
         background-color: <?php echo $veryBrightColor; ?> !important;
     }
+
     .fc-center h2 {
         color: <?php echo $calendarColor; ?>;
     }
@@ -353,11 +346,13 @@ $searchRoomsURL = get_site_url()."/wp-content/plugins/wp_book_me/public/partials
     .greyGradient p {
         color: <?php echo $fiftyPercentBrighter; ?>;
     }
+
     #services {
         color: <?php echo $fiftyPercentBrighter; ?>;
     }
+
     #roomPictureSelect {
-        border: 10px dotted  <?php echo $veryBrightColor; ?> ;
+        border: 10px dotted <?php echo $veryBrightColor; ?>;
     }
 
 </style>
@@ -372,9 +367,9 @@ $searchRoomsURL = get_site_url()."/wp-content/plugins/wp_book_me/public/partials
         </div>
 
         <div class="col-md-3">
-            <div class ="greyGradient">
-            <h3><?php echo $groupName; ?></h3>
-                <p style="text-align: center;" >
+            <div class="greyGradient">
+                <h3><?php echo $groupName; ?></h3>
+                <p style="text-align: center;">
                     <?php echo $description; ?>
                 </p>
                 <img src="http://bookme.myweb.jce.ac.il/wp-content/uploads/2016/05/meeting.png"/>
@@ -382,7 +377,11 @@ $searchRoomsURL = get_site_url()."/wp-content/plugins/wp_book_me/public/partials
                 <p></p><br>
                 <h4>אמצעים מיוחדים</h4> <br>
                 <div id=services></div>
-        
+
+                <?php if($userID ==0) : ?>
+                    <a href="http://bookme.myweb.jce.ac.il/wp-login.php?redirect_to=http%3A%2F%2Fbookme.myweb.jce.ac.il%2Fwp-admin%2F&reauth=1">לחץ כאן להתחברות</a>
+                <?php endif; ?>
+
             </div>
         </div>
 
@@ -409,8 +408,8 @@ $searchRoomsURL = get_site_url()."/wp-content/plugins/wp_book_me/public/partials
 
             <div class="col-sm-3 col-xs-6">
 
-                    חדרים
-                    <br><br>
+                חדרים
+                <br><br>
                 <div id="roomConfirmSelect">
                     <button type="button" id="btnFindRoom" class="cool-button">חיפוש</button>
 
@@ -420,17 +419,24 @@ $searchRoomsURL = get_site_url()."/wp-content/plugins/wp_book_me/public/partials
                             </select></div>
                         <button type="button" id="btnReserveRoom" class="cool-button">אישור</button>
                     </div>
-             
+
                 </div>
             </div>
 
             <div class="col-sm-3 col-xs-6 text-right">
-               שירותים <br><br>
-                <table class="table-sm text-right table-cool" style="margin-bottom: 0px;" > <tbody> <tr> <td data-halign="right"  class ="text-right"> שירות </td> <td data-halign="left">&emsp; סמן &emsp;</td> </tr> </tbody></table>
+                שירותים <br><br>
+                <table class="table-sm text-right table-cool" style="margin-bottom: 0px;">
+                    <tbody>
+                    <tr>
+                        <td data-halign="right" class="text-right"> שירות</td>
+                        <td data-halign="left">&emsp; סמן &emsp;</td>
+                    </tr>
+                    </tbody>
+                </table>
 
-                    <div id="checkboxes">
-                        <!--  here, the capabilities will be printed dynamically from js-->
-                    </div>
+                <div id="checkboxes">
+                    <!--  here, the capabilities will be printed dynamically from js-->
+                </div>
             </div>
 
             <div class="col-sm-3 col-xs-6 text-right">
@@ -439,21 +445,32 @@ $searchRoomsURL = get_site_url()."/wp-content/plugins/wp_book_me/public/partials
                         מתי להזמין<br><br>
                         <tr class="trStart">
                             <td class='tdStart'><label>שעת התחלה: </label></td>
-                            <td class='tdStart'><input id="inputStartTime" type="text" class="time labelForom"  pattern="[0-9]" maxlength="5" placeholder="##:##" data-error="input time as ##:##" onkeydown="return false"  required/>
+                            <td class='tdStart'><input id="inputStartTime" type="text" class="time labelForom"
+                                                       pattern="[0-9]" maxlength="5" placeholder="##:##"
+                                                       data-error="input time as ##:##" onkeydown="return false"
+                                                       required/>
                             </td>
                         </tr>
                         <tr class="trStart">
                             <td class='tdStart'><label>שעת סיום: </label></td>
-                            <td class='tdStart'><input id="inputEndTime" type="text" class="time labelForom" data-toggle="validator" pattern="[0-9]" maxlength="5" placeholder="##:##" data-error="input time as ##:##"  onkeydown="return false" required/></td>
+                            <td class='tdStart'><input id="inputEndTime" type="text" class="time labelForom"
+                                                       data-toggle="validator" pattern="[0-9]" maxlength="5"
+                                                       placeholder="##:##" data-error="input time as ##:##"
+                                                       onkeydown="return false" required/></td>
                         </tr>
                         <tr class="trStart">
                             <td class='tdStart'><label>תאריך:</label></td>
-                            <td class='tdStart'><input type="text" class="date start labelForom" id="datePicker" data-toggle="validator" pattern="[0-9](/)" maxlength="10" placeholder="####/##/##" data-error="input date as ##/##/####" onkeydown="return false" required/>
+                            <td class='tdStart'><input type="text" class="date start labelForom" id="datePicker"
+                                                       data-toggle="validator" pattern="[0-9](/)" maxlength="10"
+                                                       placeholder="####/##/##" data-error="input date as ##/##/####"
+                                                       onkeydown="return false" required/>
                             </td>
                         </tr>
                         <tr class="trStart">
                             <td class='tdStart'><label>כמות:</label></td>
-                            <td class='tdStart'><input id="quantity" type="text" class="time labelForom" data-toggle="validator" pattern="[0-9]" maxlength="3" required/></td>
+                            <td class='tdStart'><input id="quantity" type="text" class="time labelForom"
+                                                       data-toggle="validator" pattern="[0-9]" maxlength="3" required/>
+                            </td>
                         </tr>
                     </table>
                     <div id="errorInput"></div>
@@ -469,15 +486,17 @@ $searchRoomsURL = get_site_url()."/wp-content/plugins/wp_book_me/public/partials
                                 <button type="button" class="close" data-dismiss="modal">&times;</button>
                                 <h4 class="modal-title">פרטי החדר</h4>
                             </div>
-                            <div id = "diplayOrderRoom" class="modal-body">
-                                
+                            <div id="diplayOrderRoom" class="modal-body">
+
                             </div>
                             <div class="modal-body" id="editRoomTime">
                                 <div id="changeOrderTime">
                                     כרגע לא ידוע מה יהיה כאן
                                 </div>
                                 <br>
-                                <button id="deleteOrderButton" type="button" data-dismiss="modal" class="btn btn-success">מחיקת חדר</button>
+                                <button id="deleteOrderButton" type="button" data-dismiss="modal"
+                                        class="btn btn-success">מחיקת חדר
+                                </button>
                             </div>
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
